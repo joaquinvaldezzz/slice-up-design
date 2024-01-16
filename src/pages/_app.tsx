ce450eb334ee0f200f4da3f02625ce8a00b1d382
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-01-16 22:09:33 +0800

    Change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
       <Component {...pageProps} />
