705992403c0843eef603a9f4af7da16d61627c8f
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-01-17 16:24:04 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+// import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 import useEmblaCarousel from 'embla-carousel-react'
 import cards from '~/data/groups'
 
@@ -25,7 +26,7 @@ export default function Home(): JSX.Element {
           </div>
         </div>
 
-        <div className="container relative overflow-hidden" ref={emblaRef}>
+        <div className="container relative overflow-x-hidden" ref={emblaRef}>
           <div className="-ml-4 flex py-5">
             {cards.map((card) => (
               <div
@@ -39,6 +40,7 @@ export default function Home(): JSX.Element {
                       src={card.image}
                       alt={card.imageAlt}
                       fill
+                      sizes="100vw"
                       priority
                     />
                   </div>
@@ -54,11 +56,16 @@ export default function Home(): JSX.Element {
                       </div>
                     </div>
                     <hr className="my-4 border-t-divider" />
-                    <p className="text-lg font-medium text-dark-1">{card.description}</p>
+                    <p className="text-lg font-medium tracking-tight text-dark-1">
+                      {card.description}
+                    </p>
                     <a
-                      className="mt-7 flex items-center justify-center gap-2.5 rounded-md bg-brand-1 py-4 text-sm font-semibold text-white"
+                      className="mt-7 flex items-center justify-center gap-2.5 rounded-md bg-brand-1 py-4 text-sm font-semibold tracking-tight text-white"
                       href={card.link}
                     >
+                      <svg className="size-4.5" fill="currentColor" viewBox="0 0 512 512">
+                        <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256c0 120 82.7 220.8 194.2 248.5V334.2h-52.8V256h52.8v-33.7c0-87.1 39.4-127.5 125-127.5 16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287v175.9C413.8 494.8 512 386.9 512 256z" />
+                      </svg>
                       {card.linkText}
                     </a>
                   </div>
@@ -68,17 +75,17 @@ export default function Home(): JSX.Element {
           </div>
 
           {/* <button
-            className="absolute left-4 top-1/2 size-16 -translate-y-1/2 rounded-full bg-white text-dark-5 shadow-button"
+            className="flex size-16 items-center justify-center rounded-full bg-white text-dark-5 shadow-button xl:absolute xl:left-4 xl:top-1/2 xl:-translate-y-1/2"
             type="button"
           >
-            Prev
+            <ChevronLeftIcon className="size-6" />
           </button>
 
           <button
-            className="absolute right-4 top-1/2 size-16 -translate-y-1/2 rounded-full bg-white text-dark-5 shadow-button"
+            className="flex size-16 items-center justify-center rounded-full bg-white text-dark-5 shadow-button xl:absolute xl:right-4 xl:top-1/2 xl:-translate-y-1/2"
             type="button"
           >
-            Next
+            <ChevronRightIcon className="size-6" />
           </button> */}
         </div>
       </section>
